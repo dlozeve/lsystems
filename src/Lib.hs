f20497ecf37f6a1b4f71c0f8eb1c1859500d0be6
Dimitri Lozeve <dimitri.lozeve@gmail.com>
2018-01-15 23:25:26 +0000

    Add unit tests

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,7 @@ module Lib
     ( LSystem(..)
     , Instruction(..)
     , iterateLSystem
+    , instructions
     , drawLSystem
     ) where
 
---
+++ test/Tests.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit
+
+import Graphics.Gloss
+
+import Lib
+import Examples
+
+main :: IO ()
+main = do
+  defaultMain (testGroup "L-systems tests" [unitTests, propertyChecks])
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests"
+  [ testCase "iterate gosper once"
+  $ axiom (iterateLSystem 1 gosper) @?= "A-B--B+A++AA+B-"
+  , testCase "iterate sierpinski once"
+  $ axiom (iterateLSystem 1 sierpinski) @?= "A-B+A+B-A-BB-BB"
+  , testCase "instructions of one iteration of gosper"
+  $ instructions (iterateLSystem 1 gosper) @?= [Forward,TurnLeft,Forward,TurnLeft,TurnLeft,Forward,TurnRight,Forward,TurnRight,TurnRight,Forward,Forward,TurnRight,Forward,TurnLeft]
+  , testCase "instructions of one iteration of sierpinski"
+  $ instructions (iterateLSystem 1 sierpinski) @?= [Forward,TurnLeft,Forward,TurnRight,Forward,TurnRight,Forward,TurnLeft,Forward,TurnLeft,Forward,Forward,TurnLeft,Forward,Forward]
+  , testCase "draw axiom of gosper"
+  $ drawLSystem gosper @?= Line [(10.0,0.0),(0.0,0.0)]
+  , testCase "draw one iteration of gosper"
+  $ drawLSystem (iterateLSystem 1 gosper) @?= Line [(25.0,-8.660253),(20.0,-17.320507),(10.0,-17.320507),(-4.7683716e-7,-17.320507),(5.0,-8.660254),(15.0,-8.6602545),(10.0,0.0),(0.0,0.0)]
+  ]
+
+propertyChecks :: TestTree
+propertyChecks = testGroup "Property tests (QuickCheck)"
+  [ ]
+  
